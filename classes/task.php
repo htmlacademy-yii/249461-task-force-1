0848ac3b92c66070bea049935cb8baee45176fd3
neksen <neksen@templatemonster.me>
2021-02-19 18:21:47 +0200

    fix bugs

--- classes/task.php
+++ classes/task.php
@@ -1,30 +1,52 @@
 <?php
 
+/**
+ * Class Task - возвращает статусы и доступные действия
+ */
 class Task {
-    const STATUS_NEW = 'new';
-    const STATUS_CANCELED = 'canceled';
-    const STATUS_PROGRESS = 'progress';
-    const STATUS_COMPLETED = 'completed';
-    const STATUS_FAILED = 'failed';
 
-    const ACTION_CANCEL = 'cancel';
-    const ACTION_PERFOMED = 'perfomed';
-    const ACTION_RESPOND = 'respond';
-    const ACTION_REFUSE = 'refuse';
+    /**
+     * Константы доступных статусов
+     */
+    const STATUS_NEW = 'new';               // новая
+    const STATUS_CANCELED = 'canceled';     // отменена
+    const STATUS_PROGRESS = 'progress';     // в работе
+    const STATUS_COMPLETED = 'completed';   // выполнена
+    const STATUS_FAILED = 'failed';         // провалена
+
+    /**
+     * Константы доступных действий
+     */
+    const ACTION_CANCEL = 'cancel';         // отменить
+    const ACTION_PERFOMED = 'perfomed';     // выполнена
+    const ACTION_RESPOND = 'respond';       // откликнутся
+    const ACTION_REFUSE = 'refuse';         // отказаться
+
 
     public $currentStatus = self::STATUS_NEW;
 
     private $idExecutor;
     private $idClient;
 
+    /**
+    * Возвращает id исполнителя
+     */
     public function getIdExecutor() {
         return $this->idExecutor;
     }
 
+    /**
+     * Возвращает id заказчика
+     */
     public function getIdClient() {
         return $this->idClient;
     }
 
+    /**
+    * Конструктор принимает id заказчика и исполнителя
+     * @param int $idExecutor
+     * @param int $idClient
+     */
     public function __construct($idExecutor, $idClient) {
         $this->idExecutor = $idExecutor;
         $this->idClient = $idClient;
@@ -53,35 +75,35 @@ class Task {
         return $this->mapAction[$action];
     }
 
-    public function getStatus($action) {
+    public function getNextStatus($action) {
         switch ($action) {
             case self::ACTION_CANCEL:
-                return $this->currentStatus = self::STATUS_CANCELED;
+                return self::STATUS_CANCELED;
             case self::ACTION_PERFOMED:
-                return $this->currentStatus = self::STATUS_COMPLETED;
+                return self::STATUS_COMPLETED;
             case self::ACTION_RESPOND:
-                return $this->currentStatus = self::STATUS_PROGRESS;
+                return self::STATUS_PROGRESS;
             case self::ACTION_REFUSE:
-                return $this->currentStatus = self::STATUS_FAILED;
+                return self::STATUS_FAILED;
             default:
-                return $this->currentStatus = self::STATUS_NEW;
+                return $this->currentStatus;
         }
     }
 
-    public function getAction($currentStatus, $id) {
+    public function getAvailableAction($currentStatus, $id) {
         if ($id === self::getIdExecutor()) {
             switch ($currentStatus) {
                 case self::STATUS_NEW:
-                    return $this->mapAction[self::ACTION_RESPOND];
+                    return self::ACTION_RESPOND;
                 case self::STATUS_PROGRESS:
-                    return $this->mapAction[self::ACTION_REFUSE];
+                    return self::ACTION_REFUSE;
             }
         } elseif ($id === self::getIdClient()) {
             switch ($currentStatus) {
                 case self::STATUS_NEW:
-                    return $this->mapAction[self::ACTION_CANCEL];
+                    return self::ACTION_CANCEL;
                 case self::STATUS_PROGRESS:
-                    return $this->mapAction[self::ACTION_PERFOMED];
+                    return self::ACTION_PERFOMED;
             }
         } else {
             return print('Действие или пользователь не определены');
--- index.php
+++ index.php
@@ -3,3 +3,13 @@ set_include_path('classes');
 spl_autoload_register();
 
 $newTask = new Task(111,222);
+
+assert($newTask->getNextStatus('cancel') === Task::STATUS_CANCELED, 'cancel action');
+assert($newTask->getNextStatus('perfomed') === Task::STATUS_COMPLETED, 'perfomed action');
+assert($newTask->getNextStatus('respond') === Task::STATUS_PROGRESS, 'respond action');
+assert($newTask->getNextStatus('refuse') === Task::STATUS_FAILED, 'refuse action');
+
+assert($newTask->getAvailableAction('new', 111) === Task::ACTION_RESPOND, 'progress status');
+assert($newTask->getAvailableAction('progress', 111) === Task::ACTION_REFUSE, 'refuse status');
+assert($newTask->getAvailableAction('new', 222) === Task::ACTION_CANCEL, 'respond status');
+assert($newTask->getAvailableAction('progress', 222) === Task::ACTION_PERFOMED, 'perfomed status');
