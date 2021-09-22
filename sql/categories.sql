a77cc27b0e99733f57199de19628d39917d34c08
neksen <neksen@templatemonster.me>
2021-09-22 18:33:19 +0300

    Повелитель данных

---
+++ sql/categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (name, code) VALUES ('Переводы', 'translation'); 
+INSERT INTO categories (name, code) VALUES ('Уборка', 'clean'); 
+INSERT INTO categories (name, code) VALUES ('Переезды', 'cargo'); 
+INSERT INTO categories (name, code) VALUES ('Компьютерная помощь', 'neo'); 
+INSERT INTO categories (name, code) VALUES ('Ремонт квартирный', 'flat'); 
+INSERT INTO categories (name, code) VALUES ('Ремонт техники', 'repair'); 
+INSERT INTO categories (name, code) VALUES ('Красота', 'beauty'); 
+INSERT INTO categories (name, code) VALUES ('Фото', 'photo'); 
---
+++ sql/tasks.sql
@@ -0,0 +1,34 @@
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-03-09', '2019-11-15', 8, 2, 1, 0, 'enable impactful technologies', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', 6587); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-07-03', '2019-12-07', 8, 3, 1, 0, 'exploit revolutionary portals', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 2904); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-06-27', '2019-11-23', 8, 2, 1, 0, 'matrix next-generation e-commerce', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
+
+Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 1170); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-01-01', '2019-11-10', 8, 1, 1, 0, 'benchmark plug-and-play infomediaries', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
+
+Fusce consequat. Nulla nisl. Nunc nisl.', 838); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-09-07', '2019-12-15', 8, 3, 1, 0, 'integrate cross-platform e-business', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 7484); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2018-11-01', '2019-11-24', 8, 7, 1, 0, 'enable dot-com niches', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 5725); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-09-13', '2019-11-19', 8, 5, 1, 0, 'transform web-enabled relationships', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 4414); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-04-01', '2019-11-14', 8, 8, 1, 0, 'strategize frictionless solutions', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 3454); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-03-28', '2019-12-12', 8, 4, 1, 0, 'innovate seamless metrics', 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 3101); 
+INSERT INTO tasks (dt_add, deadline, user_id, category_id, city_id, status, name, description, cost)
+VALUES ('2019-05-01', '2019-12-19', 8, 4, 1, 0, 'integrate wireless infomediaries', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 6562); 
